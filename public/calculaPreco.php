0e7818bd0e7e6b8666b5580c8421c4538212edc8
Gabriel <augusto1793bu@gmail.com>
2023-06-14 13:47:24 -0300

    Criação do pup-up da tela de formação de preço, utilizando sweetAlert Biblioteca JS Adicionado também, o pacote Json à pasta raiz do projeto.

--- public/calculaPreco.php
+++ public/calculaPreco.php
@@ -13,34 +13,35 @@
 </head>
 
 <script>
-    const valorTotal = parseFloat(document.getElementById("valorTotal").value);
-    const porcentagemDesconto = parseFloat(document.getElementById("desconto").value);
-    const comissao = parseFloat(document.getElementById("comissao").value);
-
-    function calculaDesc() {
+    function botao() {
+        // const valorDesc = valorTotal - porcentagemDesconto;
+        // const valorComissao = valorDesc * comissao;
 
         const valorTotal = parseFloat(document.getElementById("valorTotal").value);
         const porcentagemDesconto = parseFloat(document.getElementById("desconto").value);
         const comissao = parseFloat(document.getElementById("comissao").value);
+        var valorDesc = valorTotal - porcentagemDesconto;
+        const porComi = comissao / 100;
+        const valorComissao = valorDesc * porComi;
 
-        const desconto = (valorDesc - porcentagemDesconto) / 100;
-        const valorDesc = valorTotal - desconto;
-
-        const valorComissao = valorDesc * comissao;
-
-        // alert("O novo Valor do Produto é: R$" + valorDesc)
-
-    };
+        // alert(desconto)
 
 
-    function botao() {
+
         Swal.fire({
-            title: 'Aqui Está o Cálculo:',
-            text: 'O novo valor do produto é: R$'
-        })
-    }
+            icon: 'success',
+            title: 'Aqui está o seu Cálculo:',
+            html:
+                '<b> Valor Antigo:</b> R$' + valorTotal + '<br>' +
+                '<b> Valor Com desconto:</b> R$' + valorDesc + '<br>' +
+                '<b> Total da Comissão:</b> R$ ' + valorComissao
 
-    botao();
+            //  +  'Valor Comissão: 
+
+
+        });
+
+    }
 </script>
 
 
@@ -62,24 +63,21 @@
                 <h3>Calculo de Desconto</h3>
                 <div class="mb-3">
                     <label for="valorTotal" class="form-label">Valor Total:</label>
-                    <input type="text" class="form-control" id="valorTotal" name="valorTotal" require>
+                    <input type="text" class="form-control" id="valorTotal" name="valorTotal" required="required">
                 </div>
 
                 <div class="mb-3">
                     <label class="form-label">Porcentagem Desconto:</label>
-                    <input class="form-control" id="desconto" name="desconto">
+                    <input class="form-control" id="desconto" name="desconto" required="required">
                 </div>
 
                 <div class="mb-3">
                     <label class="form-label">Comissão:</label>
-                    <input class="form-control" id="comissao" name="comissao" value="% ">
+                    <input class="form-control" id="comissao" name="comissao" required="required">
                 </div>
 
-
-
-
             </form>
-            <button type="submit" class="btn botoes" style="background-color: #f77594; color: black ; padding: 20px" onclick="botao()" data-swal-toast-template='#my-template'>Calcular Desconto</button>
+            <button type="button" class="btn botoes" style="background-color: #f77594; color: black ; padding: 20px" id="calcularDesconto" onclick="botao()">Calcular Desconto</button>
         </div>
     </div>
 
